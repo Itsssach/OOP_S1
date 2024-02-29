b4f91b1f034fcfc1bd08b1bb9a0558af02dc159e
Itsssach <161723103+Itsssach@users.noreply.github.com>
2024-02-29 12:55:00 -0500

    Update ejercicio_4.py

---
+++ ejercicio_4.py
@@ -0,0 +1,8 @@
+# Fourth exercise (14)
+
+numeric= float(input("Inserte número", )) # Allows the user to enter the desired number
+square= numeric**2 # Raised number (2)
+cubed= numeric**3 # Raised number (3)
+
+print(f"El valor del número al cuadrado es: {square}")
+print(f"El valor del número al cubo es: {cubed}")
